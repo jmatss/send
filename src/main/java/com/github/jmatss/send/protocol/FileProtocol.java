e9bfcc50ec6dbcce0b28b1e835004afdc675bc42
Jonathan Matsson <jmatss@kth.se>
2019-11-07 22:08:22 +0100

    Remove unused fields pieceSize and hashType

--- src/main/java/com/github/jmatss/send/protocol/FileProtocol.java
+++ src/main/java/com/github/jmatss/send/protocol/FileProtocol.java
@@ -10,25 +10,15 @@ import java.util.Iterator;
 public class FileProtocol extends Protocol {
     private final MessageType messageType = MessageType.FILE;
     private PFile[] files;
-    private int pieceSize;
-    private HashType hashType;
-
-    FileProtocol(PFile[] files, int pieceSize, HashType hashType) {
-        if (files.length == 0)
-            throw new IllegalArgumentException("Not allowed to create a FileProtocol with zero files");
-
-        initFileProtocol(files, pieceSize, hashType);
-    }
-
-    FileProtocol(PFile[] files, int pieceSize) {
-        this(files, pieceSize, Protocol.DEFAULT_HASH_TYPE);
-    }
 
     FileProtocol(PFile[] files) {
-        this(files, Protocol.DEFAULT_PIECE_SIZE, Protocol.DEFAULT_HASH_TYPE);
+        if (files.length == 0)
+            throw new IllegalArgumentException("Not allowed to create a FileProtocol with zero files");
+        this.files = files;
     }
 
-    FileProtocol(String[] names, String[] paths, int pieceSize, HashType hashType) throws IOException, NoSuchAlgorithmException {
+    FileProtocol(String[] names, String[] paths, HashType fileHashType) throws IOException,
+            NoSuchAlgorithmException {
         if (paths.length == 0)
             throw new IllegalArgumentException("Not allowed to create a FileProtocol with zero paths");
         else if (names.length == 0)
@@ -38,27 +28,13 @@ public class FileProtocol extends Protocol {
 
         PFile[] files = new PFile[paths.length];
         for (int i = 0; i < paths.length; i++)
-            files[i] = new PFile(names[i], paths[i], hashType);
+            files[i] = new PFile(names[i], paths[i], fileHashType);
 
-        initFileProtocol(files, pieceSize, hashType);
-    }
-
-    FileProtocol(String[] names, String[] paths, int pieceSize) throws IOException, NoSuchAlgorithmException {
-        this(names, paths, pieceSize, HashType.NONE);
+        this.files = files;
     }
 
     FileProtocol(String[] names, String[] paths) throws IOException, NoSuchAlgorithmException {
-        this(names, paths, Protocol.DEFAULT_PIECE_SIZE, HashType.NONE);
-    }
-
-    private void initFileProtocol(PFile[] files, int pieceSize, HashType hashType) {
-        if (pieceSize > Protocol.MAX_PIECE_SIZE)
-            throw new IllegalArgumentException(String.format("pieceSize > Protocol.MAX_PIECE_SIZE: (%d > %d)",
-                    pieceSize, Protocol.MAX_PIECE_SIZE));
-
-        this.files = files;
-        this.pieceSize = pieceSize;
-        this.hashType = hashType;
+        this(names, paths, Protocol.DEFAULT_HASH_TYPE);
     }
 
     @Override
@@ -66,6 +42,9 @@ public class FileProtocol extends Protocol {
         return this.messageType;
     }
 
+    /**
+     * @return an iterator over all packets to send the text.
+     */
     @Override
     public Iterable<PFile> iter() {
         return () -> new Iterator<PFile>() {
