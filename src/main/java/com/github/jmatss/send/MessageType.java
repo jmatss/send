94930e4b9756f2646b35a129afbe99bb9cf57bca
Jonathan Matsson <jmatss@kth.se>
2019-11-08 22:59:31 +0100

    Add FILE_DONE MessageType
    Will be used to indicate to the publisher that this
    subscriber have the specified file.

--- src/main/java/com/github/jmatss/send/MessageType.java
+++ src/main/java/com/github/jmatss/send/MessageType.java
@@ -1,7 +1,7 @@
 package com.github.jmatss.send;
 
 public enum MessageType {
-    PUBLISH(0), SUBSCRIBE(1), FILE(2), TEXT(3), FILE_INFO(4);
+    PUBLISH(0), SUBSCRIBE(1), FILE(2), TEXT(3), FILE_INFO(4), FILE_DONE(5);
     private final int i;
 
     MessageType(int i) {
