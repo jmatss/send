ae8a850520294a0355f517233cddda1a7c60c694
Jonathan Matsson <jmatss@kth.se>
2019-11-09 20:15:45 +0100

    Add new size to hash type and rename message types

--- src/main/java/com/github/jmatss/send/HashType.java
+++ src/main/java/com/github/jmatss/send/HashType.java
@@ -1,17 +1,25 @@
 package com.github.jmatss.send;
 
 public enum HashType {
-    NONE(0, null), SHA1(1, "SHA-1"), MD5(2, "MD5");
+    NONE(0, null, 0),
+    SHA1(1, "SHA-1", 20),
+    MD5(2, "MD5", 16);
     private final int i;
     private final String hash;
+    private final int size;
 
-    private HashType(int i, String hash) {
+    private HashType(int i, String hash, int size) {
         this.i = i;
         this.hash = hash;
+        this.size = size;
     }
 
     public int value() {
         return this.i;
+    }
+
+    public int size() {
+        return this.size;
     }
 
     public String toString() {
--- src/main/java/com/github/jmatss/send/MessageType.java
+++ src/main/java/com/github/jmatss/send/MessageType.java
@@ -1,7 +1,19 @@
 package com.github.jmatss.send;
 
 public enum MessageType {
-    PUBLISH(0), SUBSCRIBE(1), FILE(2), TEXT(3), FILE_INFO(4), FILE_DONE(5);
+    // Used when multi casting publish packet.
+    PUBLISH(0),
+    // Sent to publisher when subscriber want to receive data.
+    REQUEST(1),
+    // Used from publisher to subscriber before sending "FILE_PIECE" data.
+    FILE_INFO(2),
+    // Used when sending a "FileProtocol".
+    FILE_PIECE(3),
+    // Used when sending a "TextProtocol".
+    TEXT(4),
+    // Sent to publisher when subscriber have received specified data
+    // or sent to subscriber when publisher have no more data to send.
+    DONE(5);
     private final int i;
 
     MessageType(int i) {
