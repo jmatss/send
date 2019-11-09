2f824d6c4f378037a1ffc9529368f2e84df3be85
Jonathan Matsson <jmatss@kth.se>
2019-11-09 20:16:07 +0100

    Implement Receiver logic

--- src/main/java/com/github/jmatss/send/Receiver.java
+++ src/main/java/com/github/jmatss/send/Receiver.java
@@ -1,72 +1,136 @@
 package com.github.jmatss.send;
 
+import com.github.jmatss.send.exception.IncorrectHashTypeException;
+import com.github.jmatss.send.exception.IncorrectMessageTypeException;
+import com.github.jmatss.send.packet.FileInfoPacket;
+import com.github.jmatss.send.packet.PublishPacket;
 import com.github.jmatss.send.protocol.Protocol;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.DatagramPacket;
 import java.net.MulticastSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Set;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.locks.Lock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static com.github.jmatss.send.protocol.ProtocolActions.*;
+
 public class Receiver {
+    public static final int SOCKET_TIMEOUT = 5000; // ms
     private static final Logger LOGGER = Logger.getLogger(Receiver.class.getName());
+    private static Receiver instance;
+
+    private final ScheduledExecutorService executor;
     private final MulticastSocket socket;
     private final Set<String> subscribedTopics;
     private final Lock mutex;
-    private Receiver receiver;
+    private String downloadPath;
 
-    private Receiver(MulticastSocket socket, Set<String> subscribedTopics, Lock mutex) {
+    private Receiver(String downloadPath, MulticastSocket socket, Set<String> subscribedTopics, Lock mutex) {
+        this.downloadPath = downloadPath;
+        this.executor = ScheduledExecutorServiceSingleton.getInstance();
         this.socket = socket;
         this.subscribedTopics = subscribedTopics;
         this.mutex = mutex;
     }
 
-    /*
-    if (MessageType::PUBLISH && SubMessageType::TEXT):
-        MessageType (1 byte)
-        | Topic Length (1 byte)
-        | Topic ("Topic Length" bytes)
-        | SubMessageType (1 byte)       //
-        | Random ID Number (4 bytes)    // unique
-    if (MessageType::PUBLISH && SubMessageType::FILE):
-        MessageType (1 byte)
-        | Topic Length (1 byte)
-        | Topic ("Topic Length" bytes)
-        | SubMessageType (1 byte)
-        | Random ID Number (4 bytes) (unique)
-     */
+    public static Receiver getInstance(String downloadPath, MulticastSocket socket, Set<String> subscribedTopics,
+                                       Lock mutex) {
+        if (Receiver.instance == null)
+            Receiver.instance = new Receiver(downloadPath, socket, subscribedTopics, mutex);
+        return Receiver.instance;
+    }
 
-    public void start(MulticastSocket socket, Set<String> subscribedTopics, Lock mutex) {
-        if (receiver != null)
-            return;
-        else
-            this.receiver = new Receiver(socket, subscribedTopics, mutex);
-
+    public void start() {
         byte[] buffer = new byte[Protocol.MAX_PUBLISH_PACKET_SIZE];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
         while (true) {
             try {
-                socket.receive(packet);
-                byte[] content = Arrays.copyOfRange(packet.getData(), packet.getOffset(), packet.getLength());
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                this.socket.receive(packet);
 
-                if (content.length < Protocol.MIN_PUBLISH_PACKET_SIZE)
-                    throw new IOException("Received to few byte: " + content.length);
-                else if (content[0] != MessageType.PUBLISH.value())
+                int packetMessageType = packet.getData()[packet.getOffset()];
+                if (packet.getLength() < Protocol.MIN_PUBLISH_PACKET_SIZE)
+                    throw new IOException("Received to few byte: " + packet.getLength());
+                else if (packetMessageType != MessageType.PUBLISH.value())
                     throw new IOException("Received incorrect MessageType. Expected: "
-                            + MessageType.PUBLISH.value() + ", got: " + (int) content[0]);
+                            + MessageType.PUBLISH.value() + ", got: " + packetMessageType);
 
-                int topicLength = (int) content[1];
-                String topic = new String(content, 2, topicLength, Protocol.ENCODING);
-                int subMessageType = (int) content[2 + topicLength];
-                byte[] id = Arrays.copyOfRange(content, 2 + topicLength + 1, 2 + topicLength + 1 + 4);
-
+                this.executor.submit(() -> receive(packet));
             } catch (IOException e) {
                 LOGGER.log(Level.SEVERE, "Exception when receiving packet in Receiver: " + e.getMessage());
             }
         }
     }
 
+    private void receive(DatagramPacket packet) {
+        byte[] content = Arrays.copyOfRange(packet.getData(), packet.getOffset(), packet.getLength());
+        byte topicLength = content[1];
+        String topic;
+        int subMessageType = content[2 + topicLength];
+        int port = ByteBuffer
+                .wrap(content, topicLength + 3, 4)
+                .getInt();
+        byte[] id = Arrays.copyOfRange(content, topicLength + 7, topicLength + 11);
+
+        try {
+            topic = new String(content, 2, topicLength, Protocol.ENCODING);
+            Socket socket = new Socket(packet.getAddress(), port);
+
+            PublishPacket publishPacket = new PublishPacket(subMessageType, topicLength, topic, port, id);
+            socket.setSoTimeout(SOCKET_TIMEOUT);
+            OutputStream out = new DataOutputStream(socket.getOutputStream());
+            PushbackInputStream in = new PushbackInputStream(socket.getInputStream());
+
+            if (subMessageType == MessageType.FILE_PIECE.value())
+                receiveFile(publishPacket, socket, in, out);
+            else if (subMessageType == MessageType.TEXT.value())
+                receiveText(publishPacket, socket, in, out);
+            else
+                throw new RuntimeException("Incorrect subMessageType received: " + subMessageType);
+
+        } catch (IOException | RuntimeException | IncorrectHashTypeException | IncorrectMessageTypeException | NoSuchAlgorithmException e) {
+            // TODO: Implement better exception handling.
+            LOGGER.log(Level.SEVERE, e.getMessage());
+        } finally {
+            socket.close();
+        }
+    }
+
+    private void receiveFile(PublishPacket pp, Socket socket, PushbackInputStream in, OutputStream out)
+            throws IOException, IncorrectHashTypeException, IncorrectMessageTypeException, NoSuchAlgorithmException {
+        sendRequestPacket(out, pp);
+
+        while (true) {
+            int index = 0;
+            if (isDonePacket(in, index))
+                break;
+
+            FileInfoPacket fileInfoPacket = receiveFileInfo(in);
+
+            // If the file already exists on this local host, don't download it again.
+            // TODO: More checking, ex see if hash is the same; if not, download with another name.
+            File file = new File(this.downloadPath + fileInfoPacket.name);
+            if (!file.exists()) {
+                try (OutputStream fileWriter = new FileOutputStream(file)) {
+                    while (!isDonePacket(in, index)) {
+                        fileWriter.write(receiveFilePiece(in, pp, index));
+                        index++;
+                    }
+                }
+            } else {
+                sendDonePacket(out, index);
+            }
+        }
+    }
+
+    private void receiveText(PublishPacket publishPacket, Socket socket, PushbackInputStream in, OutputStream out)
+            throws UnsupportedEncodingException {
+
+    }
 }
