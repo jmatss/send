fdbc866bfeac32bddddc0c3172feeb15d13ff1df
Jonathan Matsson <jmatss@kth.se>
2019-11-10 16:37:05 +0100

    Add listen logic to Controller

--- src/main/java/com/github/jmatss/send/Controller.java
+++ src/main/java/com/github/jmatss/send/Controller.java
@@ -1,24 +1,20 @@
 package com.github.jmatss.send;
 
 import com.github.jmatss.send.exception.IncorrectMessageTypeException;
-import com.github.jmatss.send.protocol.FileProtocol;
-import com.github.jmatss.send.protocol.Protocol;
-import com.github.jmatss.send.protocol.TextProtocol;
+import com.github.jmatss.send.packet.RequestPacket;
+import com.github.jmatss.send.protocol.*;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.net.UnknownHostException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.io.*;
+import java.net.*;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
 import java.util.concurrent.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static com.github.jmatss.send.protocol.ProtocolActions.*;
 
 // TODO: Fix limitation of only one ip/port(?)
 public class Controller {
@@ -29,7 +25,7 @@ public class Controller {
     private int port;
 
     // PUBLISHING
-    private Map<String, Future<?>> publishedTopics;
+    private Map<String, List<Future<?>>> publishedTopics;
     private Lock mutexPublishedTopics;
 
     // SUBSCRIBING
@@ -91,7 +87,7 @@ public class Controller {
      */
     // TODO: Create listening socket in the beginning of this function and take port from it
     //  and send to getPublishPacket().
-    public String publish(Protocol protocol, String topic, long timeout, long interval) throws IncorrectMessageTypeException {
+    public String publish(Protocol protocol, String topic, long timeout, long interval) throws IncorrectMessageTypeException, IOException {
         verifyProtocol(protocol);
         if (timeout < 0)
             throw new IllegalArgumentException("Timeout set to less than zero.");
@@ -103,15 +99,25 @@ public class Controller {
             if (this.publishedTopics.containsKey(topic))
                 throw new IllegalArgumentException("Already publishing on this topic.");
 
-            byte[] packet = protocol.getPublishPacket(topic, TODO PORT);
-            DatagramPacket datagramPacket = new DatagramPacket(packet, packet.length, this.ip, this.port);
+            ServerSocket serverSocket = new ServerSocket(0);
+            Future<?> listener = this.executor.submit(
+                    () -> {
+                        try {
+                            listener(serverSocket, protocol);
+                        } catch (IOException e) {
+                            LOGGER.log(Level.SEVERE, "Exception while listening on server socket: " + e.getMessage());
+                        }
+                    }
+            );
 
-            Future<?> task = this.executor.scheduleAtFixedRate(
+            byte[] packet = protocol.getPublishPacket(topic, serverSocket.getLocalPort());
+            DatagramPacket datagramPacket = new DatagramPacket(packet, packet.length, this.ip, this.port);
+            Future<?> publisher = this.executor.scheduleAtFixedRate(
                     () -> {
                         try {
                             this.socket.send(datagramPacket);
                         } catch (IOException e) {
-                            LOGGER.log(Level.SEVERE, "Unable to send publish packet.");
+                            LOGGER.log(Level.SEVERE, "Unable to send publish packet: " + e.getMessage());
                         }
                     },
                     0,
@@ -119,7 +125,11 @@ public class Controller {
                     TimeUnit.SECONDS
             );
 
-            this.publishedTopics.put(topic, task);
+            List<Future<?>> futureList = new ArrayList<>(2);
+            futureList.add(listener);
+            futureList.add(publisher);
+
+            this.publishedTopics.put(topic, futureList);
         } finally {
             this.mutexPublishedTopics.unlock();
         }
@@ -139,18 +149,87 @@ public class Controller {
         return topic;
     }
 
+
     // FIXME: If someone does a manual cancel, and then re-published on the same topic,
     //  the "timeout-cancel" can cancel the newly published topic.
     private void cancelPublish(String topic) {
         this.mutexPublishedTopics.lock();
         try {
             if (this.publishedTopics.containsKey(topic)) {
-                this.publishedTopics.get(topic).cancel(true);
+                for (Future future : this.publishedTopics.get(topic))
+                    future.cancel(true);
                 this.publishedTopics.remove(topic);
             }
         } finally {
             this.mutexPublishedTopics.unlock();
         }
+    }
+
+    // TODO: fix so that the "send" function sends all packets before exiting if the task gets a cancel.
+    private void listener(ServerSocket serverSocket, Protocol protocol) throws IOException {
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            this.executor.submit(() -> send(clientSocket, protocol));
+        }
+    }
+
+    public void send(Socket socket, Protocol protocol) {
+        OutputStream out;
+        PushbackInputStream in;
+        try {
+            in = new PushbackInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            RequestPacket rp = receiveRequest(in);
+            this.mutexPublishedTopics.lock();
+            try {
+                if (!this.publishedTopics.containsKey(rp.topic))
+                    throw new IllegalArgumentException("Received a request with a non published topic specified : " + rp.topic);
+            } finally {
+                this.mutexPublishedTopics.unlock();
+            }
+
+            if (protocol instanceof FileProtocol)
+                sendFile(in, out, (FileProtocol) protocol);
+            else if (protocol instanceof TextProtocol)
+                sendText(out, (TextProtocol) protocol);
+            else
+                throw new RuntimeException("Incorrect protocol class");
+
+        } catch (IOException | IncorrectMessageTypeException | NoSuchAlgorithmException e) {
+            LOGGER.log(Level.SEVERE, e.getMessage());
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                LOGGER.log(Level.SEVERE, "Unable to close socket: " + e.getMessage());
+            }
+        }
+    }
+
+    private void sendFile(InputStream in, OutputStream out, FileProtocol fileProtocol)
+            throws IOException, NoSuchAlgorithmException {
+        for (PFile pfile : fileProtocol.iter()) {
+            sendFileInfo(out, pfile.getFileInfoPacket());
+
+            if (isYes(in)) {
+                // TODO: synchronize this index with the index inside filePiece.
+                int index = 0;
+                for (byte[] filePiece : pfile.packetIterator()) {
+                    sendFilePiece(out, filePiece);
+                    index++;
+                }
+                sendDone(out, index);
+            }
+        }
+        sendDone(out, 0);
+    }
+
+    private void sendText(OutputStream out, TextProtocol textProtocol) throws IOException {
+        for (byte[] textPacket : textProtocol.iter()) {
+            ProtocolActions.sendText(out, textPacket);
+        }
+        sendDone(out, 0);
     }
 
     /**
