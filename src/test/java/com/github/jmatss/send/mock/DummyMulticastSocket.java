54c45c43fd9ba2ec41e0043b00ebb2664ffb9614
Jonathan Matsson <jmatss@kth.se>
2019-11-18 23:11:03 +0100

    Add send logic to DummyMulticastSocket

--- src/test/java/com/github/jmatss/send/mock/DummyMulticastSocket.java
+++ src/test/java/com/github/jmatss/send/mock/DummyMulticastSocket.java
@@ -4,36 +4,58 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class DummyMulticastSocket extends MulticastSocket {
-    private final byte[] publish_packet;
+    // The "send" and "receive" functions will go through this.packets in order from 0 to packets.length-1.
+    private final byte[][] packets;
     private final String host;
-    private int amountOfReads;
+    private int index;
     private boolean isClosed;
 
-    public DummyMulticastSocket(byte[] publish_packet, String host, int amountOfReads) throws IOException {
-        this.publish_packet = publish_packet;
+    // Stores the packets sent via the "send" function and is received via the "receiveTest" function.
+    private BlockingQueue<DatagramPacket> sentPackets;
+
+    public DummyMulticastSocket(String host, byte[][] packets) throws IOException {
+        this.packets = packets;
         this.host = host;
-        this.amountOfReads = amountOfReads;
+        this.index = 0;
         this.isClosed = false;
+        this.sentPackets = new LinkedBlockingQueue<>();
+    }
+
+    public DummyMulticastSocket(String host) throws IOException {
+        this(host, new byte[][]{});
     }
 
     @Override
     public void receive(DatagramPacket p) throws IOException {
-        // Block indefinitely if "amountOfReads" has been read.
-        if (this.amountOfReads-- <= 0) {
-            while(true) {
-                try {
-                    Thread.sleep(Long.MAX_VALUE);
-                } catch (InterruptedException e) {
-                    return;
+        // Block indefinitely if all packets have been read.
+        if (this.index == this.packets.length) {
+                while(true) {
+                    try {
+                        Thread.sleep(Long.MAX_VALUE);
+                    } catch (InterruptedException e) {
+                        return;
+                    }
                 }
             }
-        }
 
-        p.setData(this.publish_packet.clone());
-        p.setLength(this.publish_packet.length);
+        p.setData(this.packets[this.index].clone());
+        p.setLength(this.packets[this.index].length);
         p.setAddress(InetAddress.getByName(this.host));
+
+        this.index++;
+    }
+
+    @Override
+    public void send(DatagramPacket p) {
+        this.sentPackets.add(p);
+    }
+
+    public DatagramPacket receiveTest() throws InterruptedException {
+        return this.sentPackets.take();
     }
 
     @Override
